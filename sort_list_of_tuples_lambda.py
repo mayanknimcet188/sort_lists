af33d827e73d14c2ef4f62a95a74304b1899157d
Mayank Sharma <mayank.nimcet.188@gmail.com>
2021-02-15 08:27:23 +0530

    Initial commit

---
+++ sort_list_of_tuples_lambda.py
@@ -0,0 +1,11 @@
+n = int(input())
+def split_elements(string):
+    list_element = string.split(' ')
+    return (list_element[0],int(list_element[1]))
+def take_input(n):
+    num_list = [split_elements(input()) for i in range(n)]
+    return num_list
+
+num_list = take_input(n)
+sorted_list = sorted(num_list,key = lambda tuple_element : tuple_element[1])
+print(sorted_list)
